f060bb6544a81adb9cf2e879a508ac0c19df0da1
葛海洲 <2019141410204@stu.scu.edu.cn>
2023-11-03 18:41:11 +0800

    add eef pose publish and change init pose

--- src/airbot_play_gazebo/demos/keyboard_control.py
+++ src/airbot_play_gazebo/demos/keyboard_control.py
@@ -1,6 +1,8 @@
 import rospy
 from airbot_play_control.control import RoboticArmAgent, AirbotPlayConfig
 from moveit_commander import MoveGroupCommander
+from geometry_msgs.msg import PoseStamped
+
 
 NODE_NAME = 'airbot_play_gazebo_keyboard'
 rospy.init_node(NODE_NAME)
@@ -8,18 +10,32 @@ rospy.init_node(NODE_NAME)
 other_config=("", "airbot_play_arm")
 airbot_play_arm = RoboticArmAgent(control_mode=AirbotPlayConfig.normal,init_pose=None,
                       node_name=NODE_NAME,other_config=other_config)
-airbot_play_arm.set_and_go_to_pose_target([0.702,-0.17,0.614],[0,0,0,1])
+# airbot_play_arm.set_and_go_to_pose_target([0.702,-0.17,0.614],[0,0,0,1])
+airbot_play_arm.set_and_go_to_pose_target([0.65,-0.06027,0.477],[0,1.5708,0])
 airbot_play_gripper = MoveGroupCommander("airbot_play_gripper")
 airbot_play_gripper.set_max_acceleration_scaling_factor(0.1)
 airbot_play_gripper.set_max_acceleration_scaling_factor(0.1)
+
+# 发布末端位姿话题
+eef_pose_pub = rospy.Publisher("/airbot_play/eef_pose", PoseStamped, queue_size=1)
+def publish_eef_pose():
+    rt = rospy.Rate(200)
+    while True:
+        eef_pose =  airbot_play_arm.arm_moveit.get_current_pose()
+        eef_pose_pub.publish(eef_pose)
+        rt.sleep()
+from threading import Thread
+Thread(target=publish_eef_pose,daemon=True).start()
 
 grasp_state = False
 while airbot_play_arm.is_alive():
     key = airbot_play_arm.key_control(delta_task=0.005,disable_keys=['p'],instruction=True)
     if (key == 'p'):
         if not grasp_state:
-            airbot_play_gripper.set_named_target("pick_big_cube")
-            airbot_play_gripper.go()
+            # 夹爪闭合
+            SCALE_CLOSE = 0.55
+            CLOSE = (-SCALE_CLOSE,SCALE_CLOSE,SCALE_CLOSE,-SCALE_CLOSE)
+            airbot_play_gripper.go(CLOSE)
         else:
             airbot_play_gripper.set_named_target("open")
             airbot_play_gripper.go()
