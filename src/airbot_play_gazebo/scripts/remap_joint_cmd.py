8ceb9888d40194c508503426d5ea387e4fdca554
OpenGHz <1352674740@qq.com>
2024-04-10 20:26:54 +0800

    fix remap bugs

--- src/airbot_play_gazebo/scripts/remap_joint_cmd.py
+++ src/airbot_play_gazebo/scripts/remap_joint_cmd.py
@@ -1,16 +1,32 @@
 import rospy
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64MultiArray, Float64
 
 
 class RemapJointCmd(object):
-    def __init__(self, namespace, rate=200):
+    def __init__(self, namespace, rate=200, symmetry=1):
+        """
+        namespace: str, namespace of the robot
+        rate: int, rate of the remap node
+        symmetry: float, the max gripper value
+        """
         self.rate = rate
+        self.symmetry = symmetry
+        if namespace[-1] == "/":
+            namespace = namespace[:-1]
         source_arm = f"{namespace}/joint_cmd"
         source_gripper = f"{namespace}/end_effector/command"
         target_arm = f"{namespace}/arm_group_position_controller/command"
-        target_gripper = (
-            f"{namespace}/gripper_group_position_controller/command"
+        target_gripper = f"{namespace}/gripper_group_position_controller/command"
+        target_gripper_1dof = f"{namespace}/end_effector/cmd_1dof"
+
+        self.arm_cmd_pub = rospy.Publisher(target_arm, Float64MultiArray, queue_size=10)
+        self.gripper_cmd_pub = rospy.Publisher(
+            target_gripper, Float64MultiArray, queue_size=10
+        )
+        # 将夹爪的2DOF转换为1DOF（根据对称性，限制在一定范围内）
+        self.gripper_cmd_1dof_pub = rospy.Publisher(
+            target_gripper_1dof, Float64, queue_size=10
         )
 
         self.arm_cmd_sub = rospy.Subscriber(
@@ -19,13 +35,6 @@ class RemapJointCmd(object):
         self.gripper_cmd_sub = rospy.Subscriber(
             source_gripper, JointState, self.gripper_cmd_cb
         )
-        self.arm_cmd_pub = rospy.Publisher(target_arm, Float64MultiArray, queue_size=10)
-        self.gripper_cmd_pub = rospy.Publisher(
-            target_gripper, Float64MultiArray, queue_size=10
-        )
-
-        self.arm_cmd = rospy.wait_for_message(source_arm, JointState)
-        self.gripper_cmd = rospy.wait_for_message(source_gripper, JointState)
 
     def arm_joint_cmd_cb(self, msg: JointState):
         self.arm_cmd = msg
@@ -38,6 +47,10 @@ class RemapJointCmd(object):
         target_gripper_msg = Float64MultiArray()
         target_gripper_msg.data = self.gripper_cmd.position
         self.gripper_cmd_pub.publish(target_gripper_msg)
+        # 将夹爪的2DOF转换为1DOF（根据对称性）
+        target_gripper_1dof_msg = Float64()
+        target_gripper_1dof_msg.data = target_gripper_msg.data[0] / self.symmetry
+        self.gripper_cmd_1dof_pub.publish(target_gripper_1dof_msg)
 
     def run(self):
         rate = rospy.Rate(self.rate)
@@ -54,6 +67,5 @@ class RemapJointCmd(object):
 if __name__ == "__main__":
     rospy.init_node("remap_joint_cmd")
     namespace = rospy.get_namespace()
-    remap_joint_cmd = RemapJointCmd(namespace)
-    # remap_joint_cmd.run()
+    remap_joint_cmd = RemapJointCmd(namespace, rate=200, symmetry=0.04)
     rospy.spin()
